219de622b9f04c604ffdee551444e2e9b1f08a13
MightyCoderX <mattiaca003@gmail.com>
2022-08-22 12:09:40 +0200

    Fixed formatting and console errors

--- editor/editor.js
+++ editor/editor.js
@@ -1,32 +1,39 @@
 let editor;
 
 require.config({
-    paths: {
-      vs: "../lib/monaco-editor/min/vs",
-    },
+    paths:
+    {
+      vs: '../lib/monaco-editor/min/vs',
+    }
 });
 
+window.parent.postMessage('loaded', '*');
 
 window.addEventListener('message', e =>
 {
-    require(["vs/editor/editor.main"], () => {
-        editor = monaco.editor.create(document.querySelector('.container'), {
-            value: e.data,
-            language: 'json',
-            theme: 'vs-dark',
-            scrollBeyondLastLine: true,
-            readOnly: true,
-            cursorBlinking: 'smooth',
-            dragAndDrop: true,
-            mouseWheelZoom: true,
-            wordWrap: true
+    if(e.data.name === 'json')
+    {
+        require(['vs/editor/editor.main'], () =>
+        {
+            editor = monaco.editor.create(document.querySelector('.container'), {
+                value: e.data.json,
+                language: 'json',
+                theme: 'vs-dark',
+                scrollBeyondLastLine: true,
+                readOnly: true,
+                cursorBlinking: 'smooth',
+                dragAndDrop: true,
+                mouseWheelZoom: true,
+                wordWrap: true
+            });
         });
-    });
 
-    window.addEventListener("resize", function () {
-        editor.layout();
-    });
+        window.addEventListener('resize', () =>
+        {
+            editor.layout();
+        });
+    }
 });
 
-window.parent.postMessage("loaded", "*");
+
 
--- highlighter.js
+++ highlighter.js
@@ -8,25 +8,34 @@ if(preElem && isValidJSON(preElem.innerHTML))
     document.body.style.width = '100vw';
     document.body.style.minHeight = '100vh';
     document.body.style.backgroundColor = '#1e1e1e';
-
-    console.log(`%c{%cJSONHighlighter%c}%c JSON stored in window.json, enjoy!`, 'color: blue;', 'color: aqua;', 'color: blue;', 'color: initial;');
     
     const text = preElem.innerHTML;
 
     preElem.remove();
 
-    exposeJson(text, false);
+    try
+    {
+        exposeJson(text, false);
+        console.log(`%c[%cJSONHighlighter%c]%c JSON stored in window.json, enjoy!`, 'color: #9cdcfe;', 'color: #ce9178;', 'color: #9cdcfe;', 'color: initial;');
+    }
+    catch(err)
+    {
+        console.error(err);
+    }
 
     const frame = document.createElement('iframe');
-    frame.src = chrome.runtime.getURL("/editor/editor.html");
-    frame.style = "border: 0px none; width: 100%; height: 100vh; display: block;";
+    frame.src = chrome.runtime.getURL('/editor/editor.html');
+    frame.style = 'border: 0px none; width: 100%; height: 100vh; display: block;';
     document.body.append(frame);
 
     window.addEventListener('message', e =>
     {
-        if (e.data === "loaded")
+        if (e.data === 'loaded')
         {
-            e.source.postMessage(JSON.stringify(JSON.parse(text), null, 4), chrome.runtime.getURL(""));
+            e.source.postMessage({
+                name: 'json',
+                json: JSON.stringify(JSON.parse(text), null, 4)
+            }, chrome.runtime.getURL(''));
         }
     });
 }
@@ -45,16 +54,21 @@ function isValidJSON(str)
     return true;
 }
 
-function exposeJson(text, outsideViewer) {
-    console.info("[JSONViewer] Your json was stored into 'window.json', enjoy!");
-  
-    if (outsideViewer) {
-      window.json = JSON.parse(text);
-  
-    } else {
-      var script = document.createElement("script") ;
-      script.innerHTML = 'window.json = ' + text + ';';
-      document.head.appendChild(script);
-    }
-  }
+function exposeJson(text)
+{
+    // Prints the json to console for now, until I find a way to expose it in window.json
+    window.json = JSON.parse(text);
+    console.log(json);
+
+    // Doesn't work because it violates the following Content Security Policy directive: "script-src 'self' 'wasm-unsafe-eval'".
+    // function main()
+    // {
+    //     window.json = JSON.parse(text);
+    //     console.log('bla');
+    // }
+      
+    // let script = document.createElement('script');
+    // script.appendChild(document.createTextNode('('+ main +')();'));
+    // (document.body || document.head || document.documentElement).appendChild(script);
+}
   
